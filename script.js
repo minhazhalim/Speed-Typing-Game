17c7ee0b6110b0614cc9a5676a8210be20d935f2
Minhaz Halim (Zim) <31536403+minhazhalim@users.noreply.github.com>
2023-02-16 16:10:33 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-window.addEventListener('load',init);
-const levels = {ease: 7,medium: 5,hard: 3};
-const currentLevel = levels.medium;
-let time = currentLevel;
-let score = 0;
-let isPlaying;
 const wordInput = document.querySelector('#word-input');
 const currentWord = document.querySelector('#current-word');
 const scoreDisplay = document.querySelector('#score');
@@ -12,12 +6,37 @@ const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
 const highScoreDisplay = document.querySelector('#high-score');
 const words = ['hat','river','lucky','statue','generate','stubborn','cocktail','runaway','joke','developer','establishment','hero','javascript','nutrition','revolver','echo','siblings','investigate','horrendous','symptom','laughter','magic','master','space','definition'];
-function init(){
-     seconds.innerHTML = currentLevel;
-     showWord(words);
-     wordInput.addEventListener('input',startMatch);
-     setInterval(countdown,1000);
-     setInterval(checkStatus,50);
+const levels = {ease: 7,medium: 5,hard: 3};
+const currentLevel = levels.medium;
+let time = currentLevel;
+let score = 0;
+let isPlaying;
+function showWord(words){
+     const randomIndex = Math.floor(Math.random() * words.length);
+     currentWord.innerHTML = words[randomIndex];
+}
+function matchWords(){
+     if(wordInput.value === currentWord.innerHTML){
+          message.innerHTML = 'Correct!!!!!';
+          return true;
+     }else{
+          message.innerHTML = '';
+          return false;
+     }
+}
+function countdown(){
+     if(time>0){
+          time--;
+     }else if(time === 0){
+          isPlaying = false;
+     }
+     timeDisplay.innerHTML = time;
+}
+function checkStatus(){
+     if(!isPlaying && time === 0){
+          message.innerHTML = 'Game Over!!!!!';
+          score = -1;
+     }
 }
 function startMatch(){
      if(matchWords()){
@@ -41,30 +60,11 @@ function startMatch(){
           scoreDisplay.innerHTML = score;
      }
 }
-function matchWords(){
-     if(wordInput.value === currentWord.innerHTML){
-          message.innerHTML = 'Correct!!!!!';
-          return true;
-     }else{
-          message.innerHTML = '';
-          return false;
-     }
+function init(){
+     seconds.innerHTML = currentLevel;
+     showWord(words);
+     wordInput.addEventListener('input',startMatch);
+     setInterval(countdown,1000);
+     setInterval(checkStatus,50);
 }
-function showWord(words){
-     const randomIndex = Math.floor(Math.random() * words.length);
-     currentWord.innerHTML = words[randomIndex];
-}
-function countdown(){
-     if(time>0){
-          time--;
-     }else if(time === 0){
-          isPlaying = false;
-     }
-     timeDisplay.innerHTML = time;
-}
-function checkStatus(){
-     if(!isPlaying && time === 0){
-          message.innerHTML = 'Game Over!!!!!';
-          score = -1;
-     }
-}
+window.addEventListener('load',init);
